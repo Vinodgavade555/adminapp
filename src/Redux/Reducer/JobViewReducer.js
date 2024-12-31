c4d375d7b875df8870b35ec094d41bf91e6e4146
Vinodgavade555 <Vinod.Gavade@swatsan.com>
2024-12-31 17:38:45 +0530

    "JobList Redux Structure done"

--- src/Redux/Reducer/JobViewReducer.js
+++ src/Redux/Reducer/JobViewReducer.js
@@ -1,10 +1,6 @@
 const initialState = {
-  JobApplications: [],
-  JobDetails: null, // Add this state to store job details
-  ApplyJob: null,
-  HomeData:null,
   JobList: [],
-  profiledata:null,
+
   error: null,
   isLoading: false, // Track loading for any API request
 };
@@ -18,66 +14,7 @@ const jobReducer = (state = initialState, action) => {
         isLoading: action.payload,
       };
 
-    // Job Applications Success
-    case 'JOB_APPLICATION_SUCCESS':
-      return {
-        ...state,
-        JobApplications: action.payload,
-        error: null,
-      };
-
-    // Job Applications Failure
-    case 'JOB_APPLICATION_FAILURE':
-      return {
-        ...state,
-        error: action.payload.error,
-      };
-
-    // Job Details Success
-    case 'JOB_DETAILS_SUCCESS':
-      return {
-        ...state,
-        JobDetails: action.payload, // Store job details in the state
-        error: null,
-      };
-
-    // Job Details Failure
-    case 'JOB_DETAILS_FAILURE':
-      return {
-        ...state,
-        error: action.payload.error, // Store the error message for job details
-      };
-    //job Apply Success
-    case 'JOB_APPLIED_SUCCESSFULLY':
-      return {
-        ...state,
-        ApplyJob: action.payload,
-        error: null,
-      };
-
-    // Job Apply Failure
-    case 'JOB_APPLIED_FAILURE':
-      return {
-        ...state,
-        error: action.payload.error, // Store the error message for job details
-      };
-
-    //homedata success
-    case 'JOB_HOMEDATA_SUCCESS':
-      return {
-        ...state,
-        HomeData: action.payload,
-        error: null,
-      };
-
-    // Homedata Failure
-    case 'JOB_HOMEDATA_FAILURE':
-      return {
-        ...state,
-        error: action.payload.error, // Store the error message for job details
-      };
-
-    //Job List successfull
+    //Job List
     case 'JOB_LIST_SUCCESS':
       return {
         ...state,
@@ -85,28 +22,12 @@ const jobReducer = (state = initialState, action) => {
         error: null,
       };
 
-    //Job List successfull
     case 'JOB_LIST_FAILURE':
       return {
         ...state,
         error: action.payload.error,
       };
-
-        //profiledata successfull
-    case 'PROFILE_DATA_SUCCESS':
-      return {
-        ...state,
-        profiledata: action.payload,
-        error: null,
-      };
-
-    //profiledata failure
-    case 'PROFILE_DATA_FAILURE':
-      return {
-        ...state,
-        error: action.payload.error,
-      };
-
+    // Search Job List
 
     default:
       return state;
--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,15 +1,13 @@
 import {applyMiddleware, combineReducers, createStore} from 'redux';
 import {thunk} from 'redux-thunk';
 import authReducer from './Reducer/AuthReducer';
-import JobViewController from './Action/JobViewController';
- 
+import jobReducer from './Reducer/JobViewReducer';
+
 const rootReducer = combineReducers({
   auth: authReducer,
-  Job: JobViewController,
- 
+  job: jobReducer,
 });
- 
+
 const store = createStore(rootReducer, applyMiddleware(thunk));
- 
+
 export default store;
- 
